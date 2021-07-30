a04bc07f0ee902d232696b5c43bd1beab79361b4
Morgan Laversin <morganlaversin@gmail.com>
2021-07-30 09:32:50 +0200

    function add

---
+++ app.js
@@ -0,0 +1,34 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo)
+
+// Functions
+function addTodo(event){
+    // Prevent form from submitting
+    event.preventDefault();
+    // create to do DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add('todo');
+    // create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey !';
+    newTodo.classList.add('todo-item');
+    // append to div
+    todoDiv.appendChild(newTodo);
+    // Ckeck mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    // Ckeck trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    // append to ul
+    todoList.appendChild(todoDiv);
+}
